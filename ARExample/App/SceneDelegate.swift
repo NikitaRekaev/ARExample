76959c1b92ea147cec9716d4b2a75d5156ac779c
NikitaRekaev <nvvvvk@gmail.com>
2023-04-13 18:28:03 +0400

    init project

---
+++ ARExample/App/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//  SceneDelegate.swift
+//  Nikita Rekaev 13.04.2023
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    // MARK: - Properties
+
+    var window: UIWindow?
+
+    // MARK: - Scene configuring
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let rootViewController = MainViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+
+}
---
+++ ARExample/Module/MainViewController.swift
@@ -0,0 +1,13 @@
+//  MainViewController.swift
+//  Nikita Rekaev 13.04.2023
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
